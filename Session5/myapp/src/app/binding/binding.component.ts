79e3b0deb6b3e6de4bc46c997b2c8cbccf0fb1b7
Bandils06 <hrithikbandil@gmail.com>
2022-09-16 10:19:09 +0530

    Binding Example- Prepared table in JSON format and then showing it in web page

---
+++ Session5/myapp/src/app/binding/binding.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.css']
+})
+export class BindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  products=[{name:"Product A", price:"3620", available:"Y", ratings:4.4},
+            {name:"Product B", price:"500", available:"Y", ratings:3.1},
+            {name:"Product C", price:"2220", available:"N", ratings:4.9},
+            {name:"Product D", price:"140", available:"Y", ratings:3.6},
+            {name:"Product E", price:"2100", available:"N", ratings:4.2}
+  ]
+}
